134ff07306b4154d30880fce20a4831305bd99f1
Digvijay Changdev Chavan <digvijay@localhost.localdomain>
2023-07-18 21:40:04 +0530

    Medium

---
+++ 451.SortCharactersByFrequency.cpp
@@ -0,0 +1,54 @@
+// Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in the string.
+
+// Return the sorted string. If there are multiple answers, return any of them.
+
+ 
+
+// Example 1:
+
+// Input: s = "tree"
+// Output: "eert"
+// Explanation: 'e' appears twice while 'r' and 't' both appear once.
+// So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+
+// Example 2:
+
+// Input: s = "cccaaa"
+// Output: "aaaccc"
+// Explanation: Both 'c' and 'a' appear three times, so both "cccaaa" and "aaaccc" are valid answers.
+// Note that "cacaca" is incorrect, as the same characters must be together.
+
+// Example 3:
+
+// Input: s = "Aabb"
+// Output: "bbAa"
+// Explanation: "bbaA" is also a valid answer, but "Aabb" is incorrect.
+// Note that 'A' and 'a' are treated as two different characters.
+
+ 
+
+// Constraints:
+
+//     1 <= s.length <= 5 * 105
+//     s consists of uppercase and lowercase English letters and digits.
+// ANSWER->
+class Solution {
+public:
+    string frequencySort(string s) {
+        multiset<char> mc(s.begin(),s.end());
+        set<char> sc(s.begin(),s.end());
+        vector<pair<char,int>> vec;
+        string re="";
+        for(auto ap : sc){
+            vec.push_back({ap,mc.count(ap)});
+        }
+        sort(vec.begin(),vec.end(),comp);
+        for(auto ap : vec){
+            re.append(mc.count(ap.first),ap.first);
+        }
+        return re;
+    }
+    static bool comp(pair<char,int> a,pair<char,int> b){
+        return a.second > b.second;
+    }
+};
