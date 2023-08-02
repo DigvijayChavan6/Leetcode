a6d7d142eddd8e0916762ed897bb80274b1d9d2e
Digvijay Chavan <chavandigvijay9081@gmail.com>
2023-08-02 22:38:50 +0530

    Easy

---
+++ .vscode/1207.UniqueNumberofOccurrences.cpp
@@ -0,0 +1,46 @@
+// Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: arr = [1,2,2,1,1,3]
+// Output: true
+// Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+
+// Example 2:
+
+// Input: arr = [1,2]
+// Output: false
+
+// Example 3:
+
+// Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+// Output: true
+
+ 
+
+// Constraints:
+
+//     1 <= arr.length <= 1000
+//     -1000 <= arr[i] <= 1000
+
+class Solution
+{
+public:
+    bool uniqueOccurrences(vector<int> &arr)
+    {
+        map<int, int> mp, oc;
+        for (int v : arr)
+            mp[v]++;
+        for (auto v : mp)
+        {
+            oc[v.second]++;
+            if (oc[v.second] >= 2)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
