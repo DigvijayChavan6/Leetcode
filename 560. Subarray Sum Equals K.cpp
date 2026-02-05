461045efc3eff695b4a2a604e8e042ae5e4b94df
Digvijay Chavan <chavandigvijay9081@gmail.com>
2026-02-06 00:44:51 +0530

    By using Map

---
+++ 560. Subarray Sum Equals K.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        int sum = 0, count = 0;
+        mp[sum] = 1;
+        for(int val : nums){
+            sum += val;
+            if(mp.find(sum-k) != mp.end()){
+                count += mp[sum-k];
+            }
+            mp[sum]++;
+        }
+
+        return count;
+    }
+};
