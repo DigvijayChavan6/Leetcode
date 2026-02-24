06703a33c67d63e87990ee5029bf833f46860c56
Digvijay Chavan <chavandigvijay9081@gmail.com>
2026-02-25 00:09:38 +0530

    Memoization

---
+++ 474. Ones and Zeroes.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    int getMaxLen(int len, int m, int n, vector<pair<int, int>> &cnt,
+    vector<vector<vector<int>>>& dp){
+        if(len == 0){
+            if(cnt[0].first <= m && cnt[0].second <= n){
+                return 1;
+            }else{
+                return 0;
+            }
+        }
+        if(dp[len][m][n] != -1)return dp[len][m][n];
+        int take = 0;
+        int notTake = getMaxLen(len-1, m, n, cnt, dp);
+        if(cnt[len].first <= m && cnt[len].second <= n){
+            take = 1 + getMaxLen(len-1, m-cnt[len].first, n-cnt[len].second, cnt, dp);
+        }
+        return dp[len][m][n] = max(take, notTake);
+    }
+public:
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        int len = strs.size();
+        vector<pair<int, int>> cnt;
+        for(int i = 0; i < len; i++){
+            string str = strs[i];
+            int ones = 0, zeroes = 0;
+            for(char &c : str){
+                if(c == '1')ones++;
+                else zeroes++;
+            }
+            cnt.push_back({zeroes, ones});
+        }
+        
+        vector<vector<vector<int>>> dp(len, vector<vector<int>>(m+1, vector<int>(n+1, -1)));
+
+        return getMaxLen(len-1, m, n, cnt, dp);
+    }
+};
