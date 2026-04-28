be18dde48c3d3d660c97ea23ab9302ee323e88ef
Digvijay Chavan <chavandigvijay9081@gmail.com>
2026-04-29 01:41:11 +0530

    Optimised

--- 85. Maximal Rectangle.cpp
+++ 85. Maximal Rectangle.cpp
@@ -1,41 +1,46 @@
 class Solution {
-public:
-    int largestRectangleArea(vector<int>& heights) {
+    int getMaxArea(vector<int>& heights) {
+        int n = heights.size();
+        int mxArea = 0, tp, area, prev;
         stack<int> st;
-        heights.push_back(0); // sentinel
-        int maxArea = 0;
-
-        for (int i = 0; i < heights.size(); i++) {
-            while (!st.empty() && heights[st.top()] > heights[i]) {
-                int h = heights[st.top()];
+        for(int i = 0; i < n; i++){
+            while(!st.empty() && heights[st.top()] >= heights[i]){
+                tp = st.top();
                 st.pop();
-                int width = st.empty() ? i : i - st.top() - 1;
-                maxArea = max(maxArea, h * width);
+                prev = st.empty() ? -1 : st.top();
+                area = heights[tp] * (i - prev - 1);
+                mxArea = max(area, mxArea);
             }
             st.push(i);
         }
-
-        heights.pop_back(); // remove sentinel
-        return maxArea;
-    }
-
-    int maximalRectangle(vector<vector<char>>& matrix) {
-        if (matrix.empty()) return 0;
-
-        int n = matrix[0].size();
-        vector<int> heights(n, 0);
-        int maxArea = 0;
-
-        for (auto& row : matrix) {
-            for (int j = 0; j < n; j++) {
-                if (row[j] == '1')
-                    heights[j] += 1;
-                else
-                    heights[j] = 0;
-            }
-            maxArea = max(maxArea, largestRectangleArea(heights));
+        int last = n;
+        while(!st.empty()){
+            tp = st.top();
+            st.pop();
+            prev = st.empty() ? -1 : st.top();
+            area = heights[tp] * (last - prev - 1);
+            mxArea = max(area, mxArea);
         }
 
-        return maxArea;
+        return mxArea;
+    }
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int mxArea = 0;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<int> heights(m, 0);
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(matrix[i][j] == '1'){
+                    heights[j] += 1;
+                }else{
+                    heights[j] = 0;
+                }
+            }
+            mxArea = max(mxArea, getMaxArea(heights));
+        }
+        return mxArea;
     }
 };
